f7fe77974668adb87e44a022d243527376839fab
n.kozlov <nikitacauselove@gmail.com>
2023-11-30 02:11:07 +0300

    refactor: добавить аннотацию @repository

--- src/main/java/com/sber/shoppinglist/user/UserRepository.java
+++ src/main/java/com/sber/shoppinglist/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.sber.shoppinglist.user;
 
 import com.sber.shoppinglist.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
